841d9dbf85424ff1114749657fb76bf119ced5ab
Bonnie-chris <bonifacekalama07@gmail.com>
2026-02-10 10:50:33 +0300

    new

---
+++ lesson5a.py
@@ -0,0 +1,79 @@
+# function without parameters
+# Example 1
+
+def  greeting() :
+    print("Hi")
+    print("Goodmorning")
+# called out the function 
+greeting ()
+
+# Example 2
+
+def  wedding() :
+    print("Bringing outs the gifts")
+    print("Exchanging of rings")
+    print("Giving out the speech")
+    print("Cutting the cake")
+    print("signing the marriage certificate")
+# called out the function 
+wedding()
+
+# Example 3 
+
+def graduation () :
+    print("Giving of speech")
+    print("Giving outs awards")
+    print("Drinking and eating")
+# called out the function 
+graduation ()
+
+# Example 4 
+
+def addiction () :
+    Num1=40
+    Num2=60
+    Ans=Num1+Num2
+    print("sum is",Ans)
+# called out the function 
+addiction()
+
+# Example 5
+
+def subraction () :
+    Num1=60
+    Num2=40
+    Ans=Num1-Num2
+    print("Difference is",Ans)
+# called out the function 
+subraction ()
+
+# Assingment 
+# Multiplacation
+
+def  multiplication () :
+    Num1=20
+    Num2=5
+    Ans=Num1*Num2
+    print("Product is",Ans)
+# called out the function 
+multiplication()
+
+# Division 
+
+def  division () :
+    Num1=20
+    Num2=5
+    Ans=Num1/Num2
+    print("Difference is ",Ans)
+# called out the function 
+division()
+
+# Area of a circle
+
+def area() :
+    Num1=5
+    Num2=5
+    Ans=Num1*Num2*22/7
+    print("Area is",Ans)
+# called out the function 
+area ()
---
+++ lesson5b.py
@@ -0,0 +1,64 @@
+# Function with parameters 
+# Example 1 
+
+def greeting (name) :
+    print("Goodmorning",name)
+# called out the function 
+greeting('Momo') 
+
+# Example 2 
+
+def addiction (Num1,Num2) :
+    Ans=Num1+Num2
+    print("sum",Ans)
+# called out the function 
+addiction(40,60)
+
+# NB: a function is useable block of code that perform specific task
+
+addiction (700,900)
+addiction (1500,400)
+addiction (-160,-40)
+
+# assingment 
+
+def subraction (Num1,Num2) :
+    Ans=Num1-Num2
+    print ("Difference",Ans)
+# called the function 
+subraction (-60,-40)
+
+def division (Num1,Num2) :
+    Ans=Num1/Num2
+    print("Divides",Ans)
+# called the functions 
+division(40,5)
+
+def multiplication (Num1,Num2):
+    Ans=Num1*Num2
+    print("product",Ans)
+# called the function 
+multiplication(20,12)
+# simple interest
+
+def function (p,t,r) :
+    Ans=(p*t*r)/100
+    print("Interest",Ans)
+# called out the function 
+function(24000,5,2)
+
+# area of triangle
+
+def function ( l,b,h):
+    Ans=(l*b*h)
+    print("area",Ans)
+# called the function
+function (0.5,12,10)
+
+# BMI
+
+def   calculate(w,h):
+    Ans=(w)/(h**2)
+    print("Bmi",Ans)
+# called the function 
+calculate (80,5)
